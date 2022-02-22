a423c7e07b7ff6abec0ac6e8d390894974bd8c4f
Anthony <aamay022@fiu.edu>
2022-02-22 14:49:48 -0500

    dw

--- src/components/Booknow.js
+++ src/components/Booknow.js
@@ -1,3 +1,4 @@
+import React from "react"; 
 import { useTranslation } from "react-i18next";
 const Booknow =(props)=> {
 	const { t } = useTranslation();
--- src/components/Faqs.js
+++ src/components/Faqs.js
@@ -1,3 +1,4 @@
+import React from "react"; 
 import { useTranslation } from "react-i18next";
 import './Faqs.css'
 const Faqs =(props)=> {
